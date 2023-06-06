9efcdcdc9f811556d581ecbc5d71650751770dda
robin* <robin_maki@planet.moe>
2023-06-06 20:25:21 +0900

    프로필 Create 추가

--- src/core.module.ts
+++ src/core.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import DatabaseModule from './module/database';
-import SessionModule from './module/session';
+import AuthModule from './module/auth';
 import { ConfigModule } from '@nestjs/config';
+import ProfileModule from './module/profile';
 
 @Module({
   imports: [
     DatabaseModule,
-    SessionModule,
+    AuthModule,
     ConfigModule.forRoot({ isGlobal: true }),
+    ProfileModule,
   ],
 })
 export class CoreModule {}
