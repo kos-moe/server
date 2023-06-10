228e825fbea47e15f50e8371616950d11d9ab500
robin* <robin_maki@planet.moe>
2023-06-11 00:08:41 +0900

    기존 데이터베이스 라이브러리 제거

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -1,5 +1,16 @@
 import { drizzle, PostgresJsDatabase } from 'drizzle-orm/postgres-js';
 import * as postgres from 'postgres';
+import { UpdateDeleteAction } from 'drizzle-orm/pg-core';
+
+type FKType = {
+  onUpdate: UpdateDeleteAction;
+  onDelete: UpdateDeleteAction;
+};
+
+export const CASCADE: FKType = {
+  onUpdate: 'cascade',
+  onDelete: 'cascade',
+};
 
 const queryClient = postgres(process.env.DATABASE_URL);
 const db: PostgresJsDatabase = drizzle(queryClient);
